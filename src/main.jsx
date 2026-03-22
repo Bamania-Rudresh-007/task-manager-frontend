0eb3c1a3b387a1b156f0e10d75eaf43bcaa7b5f3
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-22 10:32:00 +0530

    add routes using createBrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,28 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import { UserProvider } from "./context/User.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Login />,
+    },
+    {
+        path: "/signup",
+        element: <Signup />,
+    },
+    {
+        path: "/dashboard",
+        element: <Dashboard />,
+    },
+]);
+
+createRoot(document.getElementById("root")).render(
+    <UserProvider>
+        <RouterProvider router={router} />
+    </UserProvider>,
+);
