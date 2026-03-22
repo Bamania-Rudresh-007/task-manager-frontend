73fea696eb28ee5db84c107300251f2da3b0394a
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-22 10:32:53 +0530

    add user context to serve data across the folders

---
+++ src/context/User.jsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useState } from "react";
+
+const UserContext = createContext();
+
+export function UserProvider({children}){
+
+    const [userDetail, setUserDetail] = useState(() => {
+        try{
+            const currentUserDetail = JSON.parse(localStorage.getItem("CurrentUserDetail"));
+            if(currentUserDetail){
+                return currentUserDetail;
+            }
+            return {};
+        }
+        catch(error){
+            console.log("Failed fetching currentUserDetail from local storage Error: ", error);
+        }
+    });
+    const value = {
+        userDetail,
+        setUserDetail,
+    }
+
+    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+}
+
+function useUsers(){
+    const context = useContext(UserContext);
+
+    if(!context){
+        throw new Error("useUsers must be used within the userProvider");
+    }
+
+    return context;
+}
+
+export default useUsers;
