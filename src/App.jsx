b03ea899165a6c07e21247c70a510a99e8299ec0
Rudresh <bamaniarudresh2008@gmail.com>
2026-03-22 10:31:26 +0530

    handle routes using createBrowserRouter routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
-import Dashboard from "./pages/Dashboard"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-
 function App() {
 
   return (
     <>
-      <Signup />
     </>
   )
 }
